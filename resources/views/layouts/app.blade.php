a0020986636852cd80d450292adfed784562b66d
ASC <franksucso19@gmail.com>
2025-05-09 19:36:58 -0500

    app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,12 +38,9 @@
                         sesión</button>
                 </form>
             </ul>
-            <div class="flex gap-5">
-                <button class="p-2 hover:bg-gray-200 rounded-full flex items-center gap-2 dark:text-white hover:dark:bg-gray-900" id="toogle_boton"><span class="material-symbols-outlined">
-                    dark_mode
-                </span>Modo oscuro</button>
+
+            {{-- Boton Nav Movil --}}
             <button class="mobile-menu-btn"><i class="fas fa-bars"></i></button>
-            </div>
         </nav>
     </header>
 
